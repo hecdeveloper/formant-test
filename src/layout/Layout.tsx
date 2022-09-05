74e52383ec70226aada521f0f9bf53af27ec05be
hecdeveloper <81876540+hecdeveloper@users.noreply.github.com>
2022-09-04 23:00:09 -0500

    conditions for links

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,19 +1,27 @@
 import React from 'react';
-import { Outlet, Link } from 'react-router-dom';
+import { Outlet, Link, useLocation } from 'react-router-dom';
 const Layout = () => {
+  const location = useLocation();
+  const currentUrl = location.pathname;
+  console.log(location);
+
   return (
     <div className="md:flex md:min-h-screen">
       <div className="md:w-1/4 bg-blue-900 px-5 py-10">
         <h2 className="text-4xl font-black text-center text-white">ROBOTS </h2>
         <nav className="mt-10">
           <Link
-            className="text-white text=2xl block mt-2 hover:text-blue-300"
+            className={`${
+              currentUrl === '/robots' ? 'text-blue-300' : 'text-white'
+            } text-2xl block mt-2 hover:text-blue-300`}
             to="/robots"
           >
             Robots
           </Link>
           <Link
-            className="text-white text=2xl block mt-2 hover:text-blue-300"
+            className={`${
+              currentUrl === '/robots/new' ? 'text-blue-300' : 'text-white'
+            } text-2xl block mt-2 hover:text-blue-300`}
             to="/robots/new"
           >
             New Robot
