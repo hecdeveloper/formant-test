5a8c30fa9b7a37d2c696fb94dd54033eabbbc67c
hecdeveloper <81876540+hecdeveloper@users.noreply.github.com>
2022-09-04 22:18:30 -0500

    update links

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,9 +1,25 @@
 import React from 'react';
-import { Outlet } from 'react-router-dom';
+import { Outlet, Link } from 'react-router-dom';
 const Layout = () => {
   return (
     <div className="md:flex md:min-h-screen">
-      <div className="md:w-1/4">1</div>
+      <div className="md:w-1/4 bg-blue-900 px-5 py-10">
+        <h2 className="text-4xl font-black text-center text-white">ROBOTS </h2>
+        <nav className="mt-10">
+          <Link
+            className="text-white text=2xl block mt-2 hover:text-blue-300"
+            to="/robots"
+          >
+            Robots
+          </Link>
+          <Link
+            className="text-white text=2xl block mt-2 hover:text-blue-300"
+            to="/robots/new"
+          >
+            New Robot
+          </Link>
+        </nav>
+      </div>
       <div className="md:w-3/4">
         <Outlet />
       </div>
