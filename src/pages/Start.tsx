d65e377a85add067e12b8fab1b88e178d29a21f1
hecdeveloper <81876540+hecdeveloper@users.noreply.github.com>
2022-09-06 10:48:36 -0500

    1 file modified

--- src/pages/Start.tsx
+++ src/pages/Start.tsx
@@ -8,14 +8,29 @@ const Start = () => {
         const url = 'http://localhost:4000/robots';
         const response = await fetch(url);
         const result = await response.json();
-        console.log(result);
+        setRobots(result);
       } catch (error) {
         console.log(error);
       }
     };
     getRobotApi();
   }, []);
-  return <div>Start</div>;
+  return (
+    <>
+      <h1 className="font-black text-4xl text-blue-900">Robots</h1>
+      <p className="mt-3">Check your Robots</p>
+      <table className="w-full mt-5 table auto shadow bg-white">
+        <thead>
+          <tr>
+            <th className="border px-4 py-2">Name</th>
+            <th className="border px-4 py-2">Arms</th>
+            <th className="border px-4 py-2">Notes</th>
+          </tr>
+        </thead>
+        <tbody></tbody>
+      </table>
+    </>
+  );
 };
 
 export default Start;
