299d1d0128deb028b6436caebccc05e21a8e076a
hecdeveloper <81876540+hecdeveloper@users.noreply.github.com>
2022-09-06 00:25:52 -0500

    init validations

--- src/components/NewForm.tsx
+++ src/components/NewForm.tsx
@@ -43,22 +43,6 @@ const NewForm = () => {
                 name="arms"
               />
             </div>
-            <div className="mb-4">
-              <label
-                className="text-gray-800"
-                htmlFor="email
-            "
-              >
-                Mail
-              </label>
-              <Field
-                id="email"
-                type="email"
-                className="mt-2 block w-full p-3 bg-gray-50"
-                placeholder="Email del Cliente"
-                name="email"
-              />
-            </div>
 
             <div className="mb-4">
               <label
