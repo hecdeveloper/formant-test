c50e254cb05138c5463c0db516df66c3c191f493
hecdeveloper <81876540+hecdeveloper@users.noreply.github.com>
2022-09-06 10:42:07 -0500

    state

--- src/pages/Start.tsx
+++ src/pages/Start.tsx
@@ -1,6 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Start = () => {
+  const [robots, setRobots] = useState([]);
+  useEffect(() => {
+    const getRobotApi = async () => {
+      try {
+        const url = 'http://localhost:4000/robots';
+        const response = await fetch(url);
+        const result = await response.json();
+        console.log(result);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getRobotApi();
+  }, []);
   return <div>Start</div>;
 };
 
